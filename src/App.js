387e527f81cc6e0f83a9a7416a582b042141fa79
Adam Maziakowski <adam.maziakowski.extern@unitymedia.de>
2019-12-12 15:36:50 +0100

    useState and custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <h1> Adopt me! </h1>
+      <SearchParams />
     </div>
   );
 };
